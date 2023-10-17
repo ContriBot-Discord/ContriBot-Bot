bc95e3c4827946fd9fd8837db6eb45da58176dde
v0ltis <barnabe.havard@gmail.com>
2023-10-17 18:47:04 +0200

    commandHandler.ts now support GUILD_ID for server commands

--- src/handlers/commandHandler.ts
+++ src/handlers/commandHandler.ts
@@ -21,8 +21,16 @@ module.exports = async (client: Client) => {
     const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
     try {
-        await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: body });
-        console.log('Successfully reloaded application (/) commands.');
+        if (process.env.GUILD_ID !== undefined) {
+            await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: body });
+            console.log('Successfully reloaded application (/) commands.');
+            return;
+        } else {
+            await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: body });
+            console.log('Successfully reloaded application (/) commands.');
+
+        }
+
     }
     catch (error) {
         console.error(error);
