3968f2253d97f38f4c2f3e007d89e05e493c7d6f
v0ltis <voltis.discord@gmail.com>
2023-10-20 23:39:23 +0200

    Updated Classes

--- src/classes/Database.ts
+++ src/classes/Database.ts
@@ -8,6 +8,34 @@ export class Database{
         this.guilds = this.fetchGuilds();
     }
 
+    getGuild(id: string): Guild{
+        // Get a guild from the database
+        // If the guild does not exist, create it and return it
+        let guild = this.guilds.find(guild => guild.id === id);
+
+        if (guild == undefined) {
+            guild = this.createGuild(id);
+        }
+        // Since Database is not configured yet, return a new guild
+        return guild
+    }
+
+    createGuild(id: string): Guild{
+        // Create a guild in the database
+        // Since Database is not configured yet, return a new guild
+        let guild = new Guild(id, "en");
+
+        // The .update method sends data to the database.
+        // With that, we can make sure that the guild is created in the database
+        guild.update();
+
+        // Once created, we add the guild to the guilds array
+        this.guilds.push(guild);
+
+        // And return it
+        return guild;
+    }
+
     fetchGuilds(): Guild[]{
         // Fetch all guilds from database
 
--- src/classes/Guild.ts
+++ src/classes/Guild.ts
@@ -17,6 +17,41 @@ export class Guild{
         this.users = this.fetchUsers();
     }
 
+    getUser(id: string): User{
+        // Get a user from the database
+        // If the user does not exist, create it and return it
+        let user = this.users.find(user => user.id === id);
+
+        if (user == undefined) {
+            user = this.createUser(id);
+        }
+
+        return user
+    }
+
+    createUser(id: string): User{
+
+        // Create a guild in the database
+        // Since Database is not configured yet, return a new guild
+        let user = new User(this, id, 0, 0, this.lang);
+
+        // The .update method sends data to the database.
+        // With that, we can make sure that the user is created in the database
+        user.update();
+
+        // Once created, we add the user to the guilds array
+        this.users.push(user);
+
+        // And return it
+        return user;
+
+    }
+
+    update(): void{
+        // Update guild in database
+        // This should send all the data to the database
+    }
+
     fetchUsers(): User[]{
         // Fetch all users from database
 
