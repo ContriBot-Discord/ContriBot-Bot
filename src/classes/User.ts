6950a956c974784aaf1a4fdce2c5054a1ef5dc95
v0ltis <voltis.discord@gmail.com>
2023-10-21 13:38:45 +0200

    Added FetchInventory methods. NOT TESTED YET

--- src/classes/User.ts
+++ src/classes/User.ts
@@ -30,14 +30,37 @@ export class User{
 
     fetchInventory(): UserItem[]{
         // Fetch all items from database
+        // TODO: Make sure that this request is correct
+        const items: UserItem[] = [];
 
-        // Since Database is not configured yet, return empty array
-        return [];
+        // Join the inventory and the shop to get the name and description
+        this.#db.query("SELECT * FROM INVENTORY INNER JOIN SHOP ON INVENTORY.item_id = SHOP.item_id WHERE user_id = ? AND guild_id = ?",
+            [this.id, this.guild.id],
+            (err, result) => {
+            if (err) throw err;
+
+            result.forEach((item: any) => {
+                items.push(
+                    new UserItem(this, item.unique_item_id, item.name, item.description, item.bought_at, item.refunded, item.refunded_at, item.used, item.used_at, this.#db
+                    ));
+            });
+        }
+        )
+    return items;
+
     }
 
     update(): void{
         // Update user in database
         // This should send all the data to the database
+
+        // If the user exists in the database, it gets updated. If not, it gets created
+        this.#db.query("REPLACE INTO USER (user_id, guild_id, points, global_points, lang) VALUES (?, ?, ?, ?, ?)",
+            [this.id, this.guild.id, this.points, this.allPoints, this.lang],
+            (err, result) => {
+                if (err) throw err;
+            });
+
     }
 
 
