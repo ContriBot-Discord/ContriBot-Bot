1c0e4ddc7ac358920ef80b734ff6eae03df7ef38
v0ltis <barnabe.havard@gmail.com>
2023-10-22 21:46:56 +0200

    Updated Database.ts

--- src/classes/Database.ts
+++ src/classes/Database.ts
@@ -12,7 +12,7 @@ export class Database{
             host     : process.env.DB_HOST + ":" + process.env.DB_PORT,
             user     : process.env.DB_USERNAME,
             password : process.env.DB_PASSWORD,
-            database : process.env.DB_DATABASE
+            database : process.env.DB_NAME
         });
     }
 
