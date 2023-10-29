5d62cb23cabbfd8c5e0f933642fbf599c3055a21
v0ltis <voltis.discord@gmail.com>
2023-10-29 11:28:59 +0100

    Added resetContribPoint embed system

--- src/slashCommands/resetContribPoint.ts
+++ src/slashCommands/resetContribPoint.ts
@@ -9,6 +9,7 @@ import {
 import { SlashCommand } from "@/types";
 
 import { DB } from "@/index";
+import resetContribPoint from "@/embeds/resetContribPoint";
 
 export const command: SlashCommand = {
   name: "resetcontribpoint",
@@ -37,23 +38,7 @@ export const command: SlashCommand = {
       9
     );
 
-    const embed = new EmbedBuilder()
-      .addFields({
-        name: "<:shiny_orange_moderator:1163759368853004298> Reset points command.",
-        value: lineString,
-      })
-      .addFields({
-        name: " ",
-        value: `All of <@${memberId}>'s contribution points have been reset.`,
-      })
-      .setColor("#ff8e4d")
-      .setTimestamp();
-
-    if (all)
-      embed.spliceFields(1, 1, {
-        name: " ",
-        value: `**All of <@${memberId}>**'s total contribution points have been reset.`,
-      });
+    const embed = resetContribPoint(memberId, all);
 
     DB.getGuild(interaction.guildId!).getUser(memberId).setPoints(0, all);
 
