433831a5a1efd0bc8b33d5f919b6239ddebb4f1e
v0ltis <voltis.discord@gmail.com>
2023-11-11 13:23:18 +0100

    voiceXp now using the new system

--- src/loops/voiceXp.ts
+++ src/loops/voiceXp.ts
@@ -2,15 +2,14 @@ import {Client, GuildMember} from 'discord.js'
 import {DB} from '@/index'
 
 
-function fetchVoiceUsers(client: Client): GuildMember[] {
-    // Only invoked once, when the bot starts
-    const voiceUsers: GuildMember[] =  [];
+function fetchVoiceUsers(client: Client): void {
 
     // For each guild
     client.guilds.cache.forEach(async guild => {
 
         // For each channel
         guild.channels.cache.forEach(channel => {
+            const guild = DB.getGuild(channel.guild.id)
 
             // If the channel is a voice channel
             if (channel.isVoiceBased()) {
@@ -21,29 +20,11 @@ function fetchVoiceUsers(client: Client): GuildMember[] {
                     // If the member is not deafened or muted
                     if (!(member.voice.deaf || member.voice.mute)){
 
-                        // Add the member to the voiceUsers array
-                        voiceUsers.push(member);
+                        // We register the member as "In voice channel" so it can get voice xp
+                        guild.getUser(member.id).voiceJoinedAt = new Date()
                     }
                 });
             }
         });
     });
-
-    return voiceUsers;
-}
-
-// WIP: Not the final version
-// Next versions will be using voiceStateUpdate event instead of checking all the guilds/channels/users every 15 minutes
-export default function (client: Client, users: GuildMember[]) {
-    // Every 15 minutes
-    // Every users in the list are supposed to be eligible for voice xp
-
-    users.forEach(async user => {
-
-        const guild = DB.getGuild(user.guild.id);
-        const db_user = guild.getUser(user.id);
-
-        db_user.addVoiceXp();
-    })
-
 }
