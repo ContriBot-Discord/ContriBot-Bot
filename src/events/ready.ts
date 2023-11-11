ab7edf1206e1ba56fef8e7e414c30d583f6abaf0
v0ltis <voltis.discord@gmail.com>
2023-11-11 13:27:06 +0100

    Removed useless and complex unused code

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,12 +2,6 @@ import { BotEvent } from "@/types";
 import {
   Client,
   Events,
-  Collection,
-  Message,
-  TextChannel,
-  ForumChannel,
-  ThreadChannel,
-  MessageReaction,
 } from "discord.js";
 
 const event: BotEvent = {
@@ -15,32 +9,8 @@ const event: BotEvent = {
   once: true,
 
   async execute(client: Client) {
-    const infoChannel: TextChannel = client.channels.cache.get(
-      process.env.INFO_CHANNEL_ID!
-    ) as TextChannel;
-    let messages: Collection<string, Message> =
-      await infoChannel.messages.fetch({ limit: 30 });
-    messages.forEach((msg: Message) => {
-      msg.reactions.cache.forEach((reaction: MessageReaction) => {
-        reaction.users.fetch();
-      });
-    });
 
-    const modChannel: ForumChannel = client.channels.cache.get(
-      process.env.MOD_CHANNEL_ID!
-    ) as ForumChannel;
 
-    modChannel.threads.cache.forEach(async (thread: ThreadChannel) => {
-      let messages: Collection<string, Message> = await thread.messages.fetch({
-        limit: 16,
-        after: thread.id,
-      });
-      messages.forEach((msg: Message) => {
-        msg.reactions.cache.forEach((reaction: MessageReaction) => {
-          reaction.users.fetch();
-        });
-      });
-    });
 
     console.log(`💪 Logged in as ${client.user?.tag}`);
   },
