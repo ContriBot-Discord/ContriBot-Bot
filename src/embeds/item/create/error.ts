40a6cdf630fa577d04dbbba1a3c065d5bd648c4c
v0ltis <barnabe.havard@gmail.com>
2023-11-21 22:17:06 +0100

    Tweaked item error message

--- src/embeds/item/create/error.ts
+++ src/embeds/item/create/error.ts
@@ -10,12 +10,12 @@ export default function (lang: string): EmbedBuilder {
             {
                 name:
                     "<:shiny_red_bug:1176622435899019355> " +
-                    "Error",
-                value: "<:shiny_red_bar:1176625383211282483>".repeat(8),
+                    "Whoops, I just caught a bug !",
+                value: "<:shiny_red_bar:1176625383211282483>".repeat(11),
             },
             {
                 name: " ",
-                value: "An error occurred.\n\nPlease, remember that the `label` filed has to be less than 30 characters long and the `description` field has to be less than 150 characters long !\n\nIf you think this is a bug, please report it on the [support server](https://discord.gg/8zWZGfH) !"
+                value: "An error occurred.\n\nPlease, remember that the `label` field has to be less than 30 characters long and the `description` field has to be less than 150 characters long !\n\nIf you think this is a bug, please report it on the [support server](https://discord.gg/pw88gWrY5d) !"
             }
         )
         .setColor("#dd4040")
