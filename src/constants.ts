bcf56ea6f331eaceefc87b7f03074a090fb5bbed
Anathos <83123402+EAnathos@users.noreply.github.com>
2023-11-22 19:03:53 +0100

    Create constants.ts
    Create constants file with emojies and color to make the job easier

---
+++ src/constants.ts
@@ -0,0 +1,53 @@
+const emojies = [
+  {
+    name: "red_line",
+    value: "<:red_line:1176930932838629489>",
+  },
+  {
+    name: "orange_line",
+    value: "<:orange_line:1176931079979028530>",
+  },
+  {
+    name: "blue_line",
+    value: "<:blue_line:1176930988677410886>",
+  },
+  {
+    name: "pink_line",
+    value: "<:pink_line:1176930957656334436>",
+  },
+  {
+    name: "blue_dev",
+    value: "<:blue_dev:1176930986940973166>",
+  },
+];
+
+const colors = [
+  {
+    name: "RED",
+    hex: "#D73E3E",
+    light: false,
+  },
+  {
+    name: "PINK",
+    hex: "#FF5D99",
+    light: false,
+  },
+  {
+    name: "ORANGE",
+    hex: "#F69255",
+    light: false,
+  },
+  {
+    name: "BLUE",
+    hex: "#36DBFF",
+    light: false,
+  },
+];
+
+export const getColor = (name: string) => {
+  return colors.find((color) => color.name === name);
+};
+
+export const getEmoji = (name: string) => {
+  return emojies.find((emoji) => emoji.name === name);
+};
