3c32d31fb34b955430381858cbadd7e502d46edd
v0ltis <voltis.discord@gmail.com>
2023-11-24 22:53:02 +0100

    Minor tweaks

--- src/builders/embeds/errors/itemStringTooLong.ts
+++ src/builders/embeds/errors/itemStringTooLong.ts
@@ -15,7 +15,7 @@ export default function (lang: string, field_name: string, length: number, max_l
             },
             {
                 name: " ",
-                value: `Sorry, the field \`${field_name}\` you tried to add is too long (${length}/${max_length})!\n\nIf you think this is a bug, please report it on the [support server](https://discord.gg/pw88gWrY5d) !`
+                value: `Sorry, the field \`${field_name}\` you tried to edit is too long (${length}/${max_length} max characters)!\n\nIf you think this is a bug, please report it on the [support server](https://discord.gg/pw88gWrY5d) !`
             }
         )
         .setColor("#dd4040")
