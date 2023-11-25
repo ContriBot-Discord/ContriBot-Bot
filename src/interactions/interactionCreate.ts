8c82c6330fca42939b0de91641ee1fb0085e2e13
v0ltis <voltis.discord@gmail.com>
2023-11-25 15:37:48 +0100

    implemented new crash handler

--- src/interactions/interactionCreate.ts
+++ src/interactions/interactionCreate.ts
@@ -1,5 +1,8 @@
 import { BotEvent } from "@/types";
 import { Events, Interaction } from "discord.js";
+import ErrorHandler from "@/tools/errors";
+import error from "@/builders/embeds/error";
+import {DB} from "@/index";
 
 const event: BotEvent = {
   name: Events.InteractionCreate,
@@ -13,8 +16,30 @@ const event: BotEvent = {
 
     if (!command) return;
 
-    await command.execute(interaction);
-  },
-};
+    // Try to execute the command
+    try {
+      await command.execute(interaction);
+
+    // If an unhandled error occurs, we log it and send a message to the user
+    } catch (err) {
+      // @ts-ignore required. We do know that this is an error
+      // @ts-ignore
+      const errorId = ErrorHandler(err, DB, interaction);
+
+      try{
+        const guild = DB.getGuild(interaction.guildId || 'err');
+        const lang = guild ? guild.lang : 'en';
+
+        interaction.reply({
+            embeds: [error(lang, errorId)],
+            ephemeral: true,
+        });
+    } catch (err) {
+        // @ts-ignore
+        console.log(`[${new Date().toLocaleString()}] Failed to send error message for ID #${errorId}: ${err.message}`);
+      }
+
+  }
+}}
 
 export default event;
