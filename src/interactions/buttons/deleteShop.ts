909607e38f6c21e7d54f0434f60aa8a4852b1c0b
Anathos <83123402+EAnathos@users.noreply.github.com>
2023-11-27 15:18:15 +0100

    Create deleteShop.ts

---
+++ src/interactions/buttons/deleteShop.ts
@@ -0,0 +1,35 @@
+import itemNotFound from "@/builders/embeds/errors/itemNotFound";
+import deleteSucess from "@/builders/embeds/item/delete/deleteSucess";
+import { DB } from "@/index";
+import { BotEvent } from "@/types";
+import { Events, Interaction } from "discord.js";
+
+const event: BotEvent = {
+  name: Events.InteractionCreate,
+  once: false,
+  async execute(interaction: Interaction) {
+    if (!interaction.isButton()) return;
+
+    if (!interaction.customId.startsWith("delete")) return;
+
+    const guild = DB.getGuild(interaction.guildId!);
+
+    const item = guild.getShopItem(interaction.customId.split("-")[1]);
+
+    console.log(item)
+
+    if (item === null) {
+      await interaction.reply({
+        embeds: [itemNotFound(guild.lang)],
+        ephemeral: true,
+      });
+    } else {
+      item.delete();
+      await interaction.reply({
+        embeds: [deleteSucess(guild.lang, item.label, item.id!)],
+      });
+    }
+  },
+};
+
+export default event;
