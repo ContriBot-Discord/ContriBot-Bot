aac93c1f1fbf08a8c3f59834c0a13d757687eea0
EAnathos <Nathael.Martinelle.Etu@univ-lemans.fr>
2023-12-21 10:56:43 +0100

    refactor boostCreate strings and embed

--- src/builders/embeds/errors/items/itemCreate.ts
+++ src/builders/embeds/errors/items/itemCreate.ts
@@ -4,21 +4,22 @@ import i18next from "i18next";
 
 // WIP
 export default function (lang: string): EmbedBuilder {
-    i18next.changeLanguage(lang);
+  i18next.changeLanguage(lang);
 
-    return new EmbedBuilder()
-        .addFields(
-            {
-                name:
-                    getEmoji("red_bug") +
-                    i18next.t("errors:boost.create.title"),
-                value: getEmoji("red_line"),
-            },
-            {
-                name: " ",
-                value: i18next.t("errors:boost.create.description")
-            }
-        )
-        .setColor("#dd4040")
-        .setTimestamp();
+  return new EmbedBuilder()
+    .addFields(
+      {
+        name: getEmoji("red_bug") + i18next.t("errors:boost.create.title"),
+        value: getEmoji("red_line"),
+      },
+      {
+        name: " ",
+        value:
+          i18next.t("errors:boost.create.description") +
+          "\n\n" +
+          i18next.t("errors:default.serverReport"),
+      }
+    )
+    .setColor("#dd4040")
+    .setTimestamp();
 }
