a67dfe00dd29534c91cec919630b89b24ff774a1
EAnathos <Nathael.Martinelle.Etu@univ-lemans.fr>
2024-01-10 09:56:02 +0100

    Update guildMemberUpdate.ts

--- src/events/guildMemberUpdate.ts
+++ src/events/guildMemberUpdate.ts
@@ -1,14 +1,22 @@
 import { BotEvent } from "@/types";
 import { Events, GuildMember } from "discord.js";
+import { DB } from "..";
 
 const event: BotEvent = {
   name: Events.GuildMemberUpdate,
   once: false,
 
   async execute(oldMember: GuildMember, newMember: GuildMember) {
+    const guild = DB.getGuild(oldMember.guild.id);
+    const user = guild.getUser(newMember.id);
+
     // if a user boosts the server
     if (newMember.premiumSince) {
-      console.log(`${oldMember.user.tag} boosted the server!`);
+      user.addPoints(guild.boostPoint);
+    }
+    // If a user removes their boost
+    else if (oldMember.premiumSince && !newMember.premiumSince) {
+      user.addPoints(-guild.boostPoint);
     }
   },
 };
